f6727c98ccb72bddd5e922f3e9bdf1268ed67fa1
IvanML <ivanmartinlopez19@hotmail.com>
2018-04-04 00:17:26 +0200

    fix
    .

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = require('./app')
 const config = require('./config')
 
 mongoose.connect(config.db, (err, res) => {
-	if (err) return console.log(`Error al conectar a la BD: ${err}`)
+	if (err) return console.log(`Error al conectar a la BD: ${err}`);
 	
-	console.log(`DB connected correctly.`)
+	console.log(`DB connected correctly.`);
 
 	app.listen(config.port, () => {
-		console.log(`Server running on http://localhost:${config.port}`)
+		console.log(`Server running on http://localhost:${config.port}`);
 	})
-})
+});
